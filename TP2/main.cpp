6ece374e8691b74b8c9ec99692485284982d95f6
Tobías Vallejos <tvallejos@frba.utn.edu.ar>
2024-11-08 22:28:38 -0300

    Update main.cpp

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -4,148 +4,135 @@
 #include "rwstring.hpp"
 using namespace std;
 
-struct Lote{
+struct Registro{ //Registro
     string producto;
     string lote;
     int cantidad;
 };
 
-struct ProductoLote{
+struct LoteCantidad{ //Nivel 2
+    string lote;
+    int cantidad;
+};
+
+struct ProductoCantidad{ //Nivel 1
     string producto;
     int cantidad;
+    Nodo<LoteCantidad>* lista = nullptr;
 };
 
 const int lprod = 10;
 const int llote = 6;
 
-ostream& operator << (ostream& os, const Lote& l){
-    os << l.producto << "\t" << l.cantidad << endl;
+ostream& operator << (ostream& os, const ProductoCantidad& pc){
+    os << pc.producto << "\t" << pc.cantidad << endl;
     return os;
 };
 
-ostream& operator << (ostream& os, const ProductoLote& pl){
-    os << pl.producto << "\t" << pl.cantidad << endl;
+ostream& operator << (ostream& os, const LoteCantidad& lc){
+    os << lc.lote << "\t" << lc.cantidad << endl;
     return os;
 };
 
-
-fstream& operator << (fstream& fs, const Lote& l){
-    writestring(fs, l.producto, lprod);
-    writestring(fs, l.lote, llote);
-    fs.write(reinterpret_cast<const char *>(&l.cantidad), sizeof(l.cantidad));
+fstream& operator << (fstream& fs, Registro& reg){
+    reg.producto = readstring(fs, lprod);
+    reg.lote = readstring(fs, llote);
+    fs.read(reinterpret_cast<char *>(&reg.cantidad), sizeof(reg.cantidad));
     return fs;
 };
 
-fstream& operator >> (fstream& fs, Lote& l){
-    l.producto = readstring(fs, lprod);
-    l.lote = readstring(fs, llote);
-    fs.read(reinterpret_cast<char *>(&l.cantidad), sizeof(l.cantidad));
-    return fs;
+int criterioProductoCantidad(ProductoCantidad a, ProductoCantidad b){
+    return a.producto.compare(b.producto);
 };
 
-template <typename T> void borrarSiguiente(Nodo<T>* actual){
-    if(actual != nullptr && actual->sig != nullptr){
-        Nodo<T>* aux = actual->sig;
-        actual->sig = actual->sig->sig;
-        delete aux;
-    }
+int criterioLoteCantidad(LoteCantidad a, LoteCantidad b){
+    return b.lote.compare(a.lote);
 };
 
-template <typename T> void consolidarCantidadProductos(Nodo<T>* lista){
-    while(lista != nullptr){
-        while(lista->sig != nullptr && criterioLote(lista->dato, lista->sig->dato) == 0){
-            lista->dato.cantidad += lista->sig->dato.cantidad;
-            borrarSiguiente(lista);
-        };
-        lista = lista->sig;
-    };
-};
-
-int criterioLote(Lote a, Lote b){
-    return (a.producto < b.producto) ? -1 : (a.producto > b.producto);
-};
-
-int criterioProductoLote(ProductoLote a, ProductoLote b){
-    return (a.producto < b.producto) ? -1 : (a.producto > b.producto);
-};
-/*
-void ingresarPedido(Lote l){
-    cout << "Ingrese un nuevo pedido:" << endl;
-    cout << "Producto: ";
-    if(cin >> l.producto){
-        cout << "Cantidad: ";
-    };
-    while(cin >> l.cantidad){
-        cout << "Producto: ";
-        if(cin >> l.producto){
-            cout << "Cantidad: ";
-        };
-    };
-};
-*/
 int main(){
-    Nodo<Lote>* listaLotes = nullptr;
-    Lote lote;
-    ProductoLote prodlote;
     fstream archivo;
-    const string ruta = "C:/Users/Tochu/Desktop/AyED - TP2/Archivos/Datos.bin";
+    const string ruta = "Datos.bin";
 
-    // Punto 2
+    //Punto 2
 
     archivo.open(ruta, ios::in | ios::binary);
     if(!archivo){
-        cout << "No se pudo encontrar el archivo Datos.bin" << endl;
+        cout << "No se pudo abrir el archivo 'Datos.bin'" << endl;
         return EXIT_FAILURE;
     };
-    while(archivo >> lote){
-        insertar(lote, listaLotes, criterioLote);
+    Nodo<ProductoCantidad>* lista = nullptr;
+    Nodo<ProductoCantidad>* pnodo;
+    Registro reg;
+    ProductoCantidad pc;
+    LoteCantidad lc;
+    while(archivo << reg){
+        pc.producto = reg.producto;
+        pnodo = insertar_unico(pc, lista, criterioProductoCantidad);
+        pnodo->dato.cantidad += reg.cantidad;
+        
+        lc.lote = reg.lote;
+        lc.cantidad = reg.cantidad;
+        insertar(lc, pnodo->dato.lista, criterioLoteCantidad);
     };
     archivo.close();
 
-    // Punto 3
+    //Punto 3
 
+    cout << "Stock Inicial" << endl;
     cout << "Producto\tCantidad" << endl;
-    consolidarCantidadProductos(listaLotes);
-    mostrar(listaLotes);
+    mostrar(lista);
 
-    // Punto 4
+    //Punto 4
 
-    Nodo<ProductoLote>* listaPedidos = nullptr;
+    Nodo<ProductoCantidad>* listaPedidos = nullptr;
     cout << "Ingrese un nuevo pedido:" << endl;
     cout << "Producto: ";
-    if(cin >> prodlote.producto){
+    if(cin >> pc.producto){
         cout << "Cantidad: ";
     };
-    while(cin >> prodlote.cantidad){
-        insertar(prodlote, listaPedidos, criterioProductoLote);
+    while(cin >> pc.cantidad){
+        agregar(listaPedidos, pc);
         cout << "Producto: ";
-        if(cin >> prodlote.producto){
+        if(cin >> pc.producto){
             cout << "Cantidad: ";
         };
     };
-    
-    mostrar(listaPedidos);
-    Nodo<Lote>* aux = listaLotes;
 
-//Se muestra solo el último objeto de la lista
+    mostrar(listaPedidos); //Después lo borro.
+    Nodo<LoteCantidad>* listaStockDespachado = nullptr;
 
-    while(listaLotes != nullptr){
-        if(listaLotes->dato.producto == listaPedidos->dato.producto){
-            if(listaLotes->dato.cantidad >= listaPedidos->dato.cantidad){
-                listaLotes->dato.cantidad =- listaPedidos->dato.cantidad;
-                cout << "Pedido despachado:" << endl;
-                cout << "Producto: " << prodlote.producto << " - Cantidad: " << prodlote.cantidad << " - Detalle de los lotes:" << endl;
-                //if(listaLotes->dato.lote)
-            } else {
-                int cantidadRestante = listaPedidos->dato.cantidad - listaLotes->dato.cantidad;
-                cout << "Pedido rechazado. Listado de faltantes:" << endl;
-                cout << listaPedidos->dato.producto << "\t" << cantidadRestante << endl;
-                
+    while(listaPedidos != nullptr){
+        while(lista != nullptr){
+            if(listaPedidos->dato.producto == lista->dato.producto){
+                int cantidadLotes = 0;
+                if(lista->dato.cantidad >= listaPedidos->dato.cantidad){
+                    lista->dato.cantidad -= listaPedidos->dato.cantidad;
+                    while(cantidadLotes < listaPedidos->dato.cantidad && lista->dato.lista != nullptr){
+                        if(lista->dato.lista->sig != nullptr){
+                            cantidadLotes += lista->dato.lista->dato.cantidad;
+                            lc.lote = lista->dato.lista->dato.lote;
+                            lc.cantidad = lista->dato.lista->dato.cantidad;
+                            if(cantidadLotes > listaPedidos->dato.cantidad){
+                                lc.lote = lista->dato.lista->dato.lote;
+                                lc.cantidad = listaPedidos->dato.cantidad - (cantidadLotes - lista->dato.lista->dato.cantidad);
+                            };
+                            agregar(listaStockDespachado, lc);
+                            lista->dato.lista = lista->dato.lista->sig;
+                        };
+                    };
+                    cout << "Pedido despachado" << endl;
+                    cout << "Producto: " << listaPedidos->dato.producto << " - Cantidad total: " << listaPedidos->dato.cantidad << " - Detalle de los lotes:" << endl;
+                    mostrar(listaStockDespachado);
+                } else {
+                    int cantidadFaltante = listaPedidos->dato.cantidad - lista->dato.cantidad;
+                    cout << "Pedido rechazado. Listado de faltantes" << endl;
+                    cout << listaPedidos->dato.producto << "\t" << cantidadFaltante << endl;
+                };
             };
-            listaPedidos = listaPedidos->sig;
+            lista = lista->sig;
         };
-        listaLotes = listaLotes->sig;
+        listaPedidos = listaPedidos->sig;
     };
 
-    return 0;
+    return 0; //POR AHORA SOLO FUNCIONA PARA EL PRIMER REGISTRO DEL PRIMER PEDIDO.
 }
